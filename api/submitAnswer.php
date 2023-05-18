89579938d8a7ffac3e906be6de15f75ccea4003c
112219 <100996992+OskarSy@users.noreply.github.com>
2023-05-18 21:22:12 +0200

    oskar - someone fix logic in submitAnswer

--- api/submitAnswer.php
+++ api/submitAnswer.php
@@ -16,6 +16,8 @@ $studentId=$_SESSION["studentId"];
 $assignmentId =  $_POST['submittedId'];
 $submittedAnswer = $_POST['submittedAnswer']; 
 
+$generationIndex = $_SESSION['generationIndex'];
+/*
 $stmt = $conn->prepare("SELECT submittedAnswer FROM StudentAssignmentLink WHERE studentId = ? AND assignmentId = ?");
 $stmt->bind_param('ss', $studentId, $assignmentId);
 $stmt->execute();
@@ -23,16 +25,66 @@ $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $submittedAnswerCheck = $row['submittedAnswer'];
+    $submittedAnswer2 = $row['submittedAnswer'];
 }
+if($submittedAnswer2==null){$notNow=false;}
+else if($submittedAnswer=$submittedAnswer2){
+    $notNow=true;
+}else{
+    $notNow=false;
+}
+*/
 
-$stmt = $conn->prepare("UPDATE StudentAssignmentLink SET submittedAnswer = ? WHERE studentId = ? AND assignmentId = ?");
-$stmt->bind_param('sss', $submittedAnswer, $studentId, $assignmentId);
+
+$stmt = $conn->prepare("UPDATE StudentAssignmentLink SET submittedAnswer = ? WHERE studentId = ? AND assignmentId = ? and generationIndex=?");
+$stmt->bind_param('ssss', $submittedAnswer, $studentId, $assignmentId,$generationIndex);
 $stmt->execute();
 
-if($submittedAnswerCheck==null){
 $stmt = $conn->prepare("UPDATE Student SET submittedCount = submittedCount + 1 WHERE id = ?");
 $stmt->bind_param('s', $studentId);
 $stmt->execute();
+
+
+
+$stmt = $conn->prepare("SELECT correctAnswer,maxPoints FROM Assignments WHERE id = ?");
+$stmt->bind_param('s',$assignmentId);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $correctAnswer = $row['correctAnswer'];
+    $maxPoints=$row['maxPoints'];
 }
+
+
+
+if($correctAnswer==$submittedAnswer){
+    $stmt = $conn->prepare("UPDATE StudentAssignmentLink SET achievedPoints = ? WHERE assignmentId = ? and studentId = ? and generationIndex=?");
+    $stmt->bind_param('ssss',$maxPoints,$assignmentId, $studentId,$generationIndex);
+    $stmt->execute();
+}
+
+$stmt = $conn->prepare("SELECT achievedPoints FROM StudentAssignmentLink WHERE studentId = ? AND generationIndex = ?");
+$stmt->bind_param('ss', $studentId, $generationIndex);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$totalAchievedPoints = 0; 
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $achievedPoints = $row['achievedPoints'];
+
+        if ($achievedPoints !== null) {
+            $totalAchievedPoints += $achievedPoints;
+        }
+    }
+}
+
+$stmt = $conn->prepare("UPDATE Student SET points=? WHERE id = ?");
+$stmt->bind_param('ss', $totalAchievedPoints,$studentId);
+$stmt->execute();
+
+
 ?>
